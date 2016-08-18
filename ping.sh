e17e1194a3a4cef2b56ee7fe4ff8d536aad08b8d
David Carboni <david@carboni.io>
2016-08-18 12:33:04 +0100

    Updated to ping heroku.

--- ping.sh
+++ ping.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+url=https://sdc-login-user.herokuapp.com
+
 while [ true ]
 do
-    curl -i \
-    -H "Content-Type: application/json" \
-    -X POST -d '{"username":"xyz","password":"xyz"}' \
-    http://localhost:5000/profile
-    
+
+  echo ---
+  echo User:
+  curl -i -X POST -H "Content-Type: application/json" \
+    -d '{"email": "user@example.com"}' \
+    ${url}/login
+
+  echo ---
+  echo Code:
+  curl -i -X POST -H "Content-Type: application/json" \
+    -d '{"code": "abc123"}' \
+    ${url}/code
+
     sleep 1
 done
