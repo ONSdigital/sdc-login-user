f1b3b46d1d3692f555dd022b575aa0625c645e25
Nick Gravgaard <me@nickgravgaard.com>
2016-11-07 11:33:11 +0000

    Change default port to 5009 as specified in sdc-frontstage-ui

--- app.py
+++ app.py
@@ -308,6 +308,6 @@ if __name__ == '__main__':
     create_users()
 
     # Start server
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 5009))
     app.run(debug=True, host='0.0.0.0', port=port)
 
