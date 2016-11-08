eb9e0e81817beb0c0c27c4df1bd27070fb9606cd
David Carboni <david@carboni.io>
2016-11-08 14:22:59 +0000

    Enabled threading in Flask (forwarded to the underlying Werkzeug).

--- app.py
+++ app.py
@@ -310,5 +310,5 @@ if __name__ == '__main__':
 
     # Start server
     port = int(os.environ.get('PORT', 5009))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port, threaded=True)
 
